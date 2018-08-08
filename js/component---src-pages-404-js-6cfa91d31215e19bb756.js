45a7a04b65d5fbdffc8cb549736331bcb2dbcdbf
Beace.Li <beace.li@trustasia.com>
2018-08-08 14:03:15 +0800

    Updates

---
+++ js/component---src-pages-404-js-6cfa91d31215e19bb756.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{211:function(e,t,n){"use strict";n.r(t);var a=n(1),u=n.n(a);t.default=function(){return u.a.createElement("div",null,u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-6cfa91d31215e19bb756.js.map
