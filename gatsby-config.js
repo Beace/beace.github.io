7901b858fe025ee24c944d9cb4f1c45fa3f827b8
BeAce <beaceshimin@gmail.com>
2019-03-22 16:18:18 +0800

    feat: add hotjar plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,8 @@ module.exports = {
   siteMetadata: {
     title: 'Beace Blog',
     author: 'Beace Lee',
-    description: 'beacelee,beacelee的博客,web前端,前端开发,react技术栈,scala学习,playframework学习',
+    description:
+      'beacelee,beacelee的博客,web前端,前端开发,react技术栈,scala学习,playframework学习',
     siteUrl: 'https://beace.github.io/',
   },
   // pathPrefix: '/gatsby-starter-blog',
@@ -10,19 +11,26 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: "Beace Blog",
-        short_name: "Beace",
-        start_url: "/",
-        background_color: "#6b37bf",
-        theme_color: "#6b37bf",
+        name: 'Beace Blog',
+        short_name: 'Beace',
+        start_url: '/',
+        background_color: '#6b37bf',
+        theme_color: '#6b37bf',
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: "standalone",
-        icon: "src/images/icon.jpg", // This path is relative to the root of the site.
+        display: 'standalone',
+        icon: 'src/images/icon.jpg', // This path is relative to the root of the site.
         // An optional attribute which provides support for CORS check.
         // If you do not provide a crossOrigin option, it will skip CORS for manifest.
         // Any invalid keyword or empty string defaults to `anonymous`
         crossOrigin: `use-credentials`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-hotjar`,
+      options: {
+        id: 558022,
+        sv: 6,
       },
     },
     {
@@ -61,7 +69,7 @@ module.exports = {
       options: {
         head: true,
         //trackingId: `ADD YOUR TRACKING ID HERE`,
-        trackingId: "UA-83986231-3",
+        trackingId: 'UA-83986231-3',
       },
     },
     `gatsby-plugin-feed`,
